5bcab3a17d49811dde022c16283ed58237324a45
BlueCP <raekbote@gmail.com>
2022-11-19 20:00:29 +0000

    Initial PC testbench

--- program_counter/pc_tb.cpp
+++ program_counter/pc_tb.cpp
@@ -1,15 +1,14 @@
-#include "Vtop.h"
+#include "Vtop_PC.h"
 
 #include "verilated.h"
 #include "verilated_vcd_c.h"
-#include "vbuddy.cpp"
 
 int main(int argc, char **argv, char **env) {
 
     Verilated::commandArgs(argc, argv);
 
     // Init top verilog instance
-    Vtop* top = new Vtop;
+    Vtop_PC* top = new Vtop_PC;
 
     // Init trace dump
     Verilated::traceEverOn(true);
@@ -17,18 +16,16 @@ int main(int argc, char **argv, char **env) {
     top->trace(tfp, 99);
     tfp->open("top.vcd");
 
-    // Init Vbuddy
-    if (vbdOpen() != 1)
-        return(-1);
-    vbdHeader("Lab 4: Reduced RISC-V");
-    vbdSetMode(0); // Toggle mode
-
     // Init simulation inputs
     top->clk = 1;
-    top->rst = 1;
+    top->rst = 0;
+    top->ImmOP = 0;
+    top->PCsrc = 0;
 
     int clk;
-    for (int i = 0; i < 1000000; i ++) {
+    int testState = 0;
+    int testCount = 0;
+    for (int i = 0; i < 1000; i ++) {
         
         // Dump variables into VCD file and toggle clock
         for (clk = 0; clk < 2; clk ++) {
@@ -37,20 +34,19 @@ int main(int argc, char **argv, char **env) {
             top->eval();
         }
 
-        // Send data to Vbuddy
-        vbdCycle(i);
-        vbdHex(1, top->a0 & 0xf);
-        vbdHex(2, (top->a0 >> 4) & 0xf);
-        vbdHex(3, (top->a0 >> 8) & 0xf);
-        vbdHex(4, (top->a0 >> 12) & 0xf);
-        // Although the register a0 is 32 bits, we will not be using it to store any numbers higher than 16 bits.
-
         // Update input signals
-        // ...
-        
+        if (testCount == 20) { // Test each input state for 20 cycles
+            testCount = 0;
+            testState ++;
+        } else {
+            top->rst = testState & 1; // 1 or 0
+            top->PCsrc = (testState >> 1) & 1; // 1 or 0
+            top->ImmOP = (testState >> 2); // N-2 bit number
+            testCount ++;
+        }
+
     }
 
-    vbdClose();
     tfp->close();
     exit(0);
 
